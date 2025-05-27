2ca049c09d4404ca7128a7770e49e227d79ac11f
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-27 22:24:03 +0300

    Update tp-client_menu.lua

--- tpz_guarma/client/tp-client_menu.lua
+++ tpz_guarma/client/tp-client_menu.lua
@@ -7,6 +7,9 @@ end)
 
 OpenedMenu = false
 
+-----------------------------------------------------------
+--[[ Functions  ]]--
+-----------------------------------------------------------
 
 function OpenTravellingMenu(index)
     OpenedMenu = true
